9f7ea8792bc760ffa2b7eceac74ccd85367f0fa1
Priyanshu-010 <priyanshurai2772@gmail.com>
2025-08-03 19:25:18 +0530

    Frontend Setup completed

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,15 @@
+// tailwind.config.js
+import daisyui from "daisyui";
+
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,jsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  darkMode: "class",
+  plugins: [daisyui],
+};
